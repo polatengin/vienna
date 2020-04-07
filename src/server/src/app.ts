eb5e393c35c5a475912e23b6724e2c8287ad4417
polatengin <enpolat@microsoft.com>
2020-04-07 12:27:33 +0300

    app starts listening from the port

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -2,3 +2,10 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on http://localhost:${port}`);
+});
