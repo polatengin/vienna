e8db7349204661b35df16b51e5526f55eaa652d3
polatengin <enpolat@microsoft.com>
2020-04-07 12:28:03 +0300

    initial implementation of root endpoint with get method

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Vienna is working 🥳');
+});
+
 app.listen(port, err => {
   if (err) {
     return console.error(err);
