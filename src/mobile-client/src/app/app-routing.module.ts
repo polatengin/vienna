0cf88d10741cb5834f7d6c9cfb33045eb623d999
polatengin <enpolat@microsoft.com>
2020-04-07 21:51:19 +0300

    initial routing module

---
+++ src/mobile-client/src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+@NgModule({
+  imports: [
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
