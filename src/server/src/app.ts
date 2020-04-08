a5736e74efcca928c24f9a6ffef6f3482ba7cdc0
polatengin <enpolat@microsoft.com>
2020-04-08 10:22:34 +0300

    honor port environment variable

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -6,7 +6,7 @@ import { MongoConnection, DoctorsCollection } from './db.service';
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyparser.json());
 
