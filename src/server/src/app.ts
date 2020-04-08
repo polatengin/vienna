53a86fb9afc5657ca372292295030b1263e04617
polatengin <enpolat@microsoft.com>
2020-04-08 09:05:03 +0300

    dbservice structures imported

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as bodyparser from 'body-parser';
 
+import { MongoConnection, DoctorsCollection } from './db.service';
 const app = express();
 const port = 3000;
 
