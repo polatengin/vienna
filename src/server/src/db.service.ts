d519baf1f571ae3fa97fedcab6f7c6e761d457e7
polatengin <enpolat@microsoft.com>
2020-04-08 09:03:29 +0300

    connection established to the mongo server

--- src/server/src/db.service.ts
+++ src/server/src/db.service.ts
@@ -10,4 +10,11 @@ export const DoctorsCollection = mongoose.model("Doctor", DoctorSchema);
 
 export class MongoConnection {
 
+  public init(username: string, password: string) {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(`mongodb://${username}:${password}@ds040309.mlab.com:40309/vienna`, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => {
+      console.log('MongoConnection established...');
+    });
+  }
+
 }
