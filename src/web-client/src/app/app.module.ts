83880116fe97f19142792848adff43dd014024cb
polatengin <enpolat@microsoft.com>
2020-04-09 19:10:46 +0300

    httpclient module imported

--- src/web-client/src/app/app.module.ts
+++ src/web-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ThemeModule } from './theme.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
     ThemeModule
   ],
