37be96b9cd529244fe107eb4eca831b57f0d1e78
polatengin <enpolat@microsoft.com>
2020-04-09 19:42:03 +0300

    fqdn of the backend api

--- src/web-client/src/app/services/api.service.ts
+++ src/web-client/src/app/services/api.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { LoginRequestModel, LoginResponseModel } from '../models/api-models';
 import { Observable } from 'rxjs';
 
+const BASE: string = 'http://localhost:3000/';
+
 @Injectable()
 export class ApiService {
   constructor(private http: HttpClient) { }
