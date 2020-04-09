4336ed3137d1b75ff88c31b2db6419d9641a04fc
polatengin <enpolat@microsoft.com>
2020-04-09 19:09:59 +0300

    theme module imported

--- src/web-client/src/app/app.module.ts
+++ src/web-client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ThemeModule } from './theme.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,8 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    ThemeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
