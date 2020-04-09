145ef11d03bf94f216b7d6889d5cc08d9105eec2
polatengin <enpolat@microsoft.com>
2020-04-09 19:08:35 +0300

    dialog module exported

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
   ]
 })
 export class ThemeModule { }
