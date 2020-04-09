d8e6504dde9c42699135b6da16916fc459c83c5a
polatengin <enpolat@microsoft.com>
2020-04-09 19:11:50 +0300

    app will open with login endpoint

--- src/web-client/src/app/app.module.ts
+++ src/web-client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ThemeModule } from './theme.module';
 import { AppComponent } from './app.component';
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
