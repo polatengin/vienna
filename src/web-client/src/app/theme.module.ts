54f43823df43d245c25583ea815fe3059f036a52
polatengin <enpolat@microsoft.com>
2020-04-09 19:08:49 +0300

    icon module exported

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
+    MatIconModule,
   ]
 })
 export class ThemeModule { }
