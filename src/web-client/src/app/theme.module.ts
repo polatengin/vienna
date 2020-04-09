4a34378231862e9a454463743fb460eb5ff1bf12
polatengin <enpolat@microsoft.com>
2020-04-09 19:09:23 +0300

    input module exported

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -4,12 +4,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
     MatDialogModule,
     MatIconModule,
+    MatInputModule,
   ]
 })
 export class ThemeModule { }
