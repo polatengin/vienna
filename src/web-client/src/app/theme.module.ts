aa146e4636ebe7973736630d3de1d97e672a3ebc
polatengin <enpolat@microsoft.com>
2020-04-09 19:09:35 +0300

    toolbar module exported

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -5,6 +5,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   exports: [
     MatButtonModule,
@@ -12,6 +14,7 @@ import { MatInputModule } from '@angular/material/input';
     MatDialogModule,
     MatIconModule,
     MatInputModule,
+    MatToolbarModule
   ]
 })
 export class ThemeModule { }
