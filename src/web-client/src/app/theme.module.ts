804d4af61dc947f8862c239d68ea5aa09e070aa0
polatengin <enpolat@microsoft.com>
2020-04-09 19:08:24 +0300

    card module exported

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
   ]
 })
 export class ThemeModule { }
