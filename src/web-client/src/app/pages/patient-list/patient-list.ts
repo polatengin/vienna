e7c8655e6d275880c11f18cc2eefa6a520dcdd3c
polatengin <enpolat@microsoft.com>
2020-04-10 00:15:55 +0300

    patient list page component

---
+++ src/web-client/src/app/pages/patient-list/patient-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-list',
+  templateUrl: './patient-list.html',
+  styleUrls: ['./patient-list.scss']
+})
+export class PatientListPageComponent {
+
+}
