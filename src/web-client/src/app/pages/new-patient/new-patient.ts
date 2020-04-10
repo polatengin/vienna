22e4f2a670b8b55cf41dc6fc24c8dbd45c6d6d9a
polatengin <enpolat@microsoft.com>
2020-04-10 12:30:21 +0300

    new patient page component created

---
+++ src/web-client/src/app/pages/new-patient/new-patient.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+import { NewPatientRequestModel } from '../../models/api-models';
+
+@Component({
+  selector: 'app-new-patient',
+  templateUrl: './new-patient.html',
+  styleUrls: ['./new-patient.scss']
+})
+export class NewPatientPageComponent {
+
+  request: NewPatientRequestModel = new NewPatientRequestModel();
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('New Patient');
+    layout.showMenu();
+    layout.showBackButton();
+  }
+
+}
