cd9de53f4e192122bc65e20bd9c26e87656b8683
saygin_ates <saygin_ates@aygaz.com.tr>
2020-04-12 13:47:02 +0000

    atlas mongodb ye göre düzenlendi

--- src/server/src/db.service.ts
+++ src/server/src/db.service.ts
@@ -1,5 +1,6 @@
-import mongoose from 'mongoose';
-
+//import mongoose from 'mongoose';
+const MongoClient = require('mongodb').MongoClient;
+/*
 const DoctorSchema = new mongoose.Schema({
   fullName: String,
   phoneNumber: String,
@@ -7,14 +8,22 @@ const DoctorSchema = new mongoose.Schema({
   isActive: Boolean,
 });
 export const DoctorsCollection = mongoose.model("Doctor", DoctorSchema);
-
+*/
 export class MongoConnection {
 
   public init(username: string, password: string) {
-    mongoose.Promise = global.Promise;
+    const uri = "mongodb+srv://${username}:${password}@vienna-cluster-tzrt1.mongodb.net/test?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    client.connect(err => {
+      const collection = client.db("Kovitakip").collection("Doctor");
+  // perform actions on the collection object
+      client.close();
+    });
+
+    /*mongoose.Promise = global.Promise;
     mongoose.connect(`mongodb://${username}:${password}@ds040309.mlab.com:40309/vienna`, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => {
       console.log('MongoConnection established...');
-    });
+    });*/
   }
 
 }
