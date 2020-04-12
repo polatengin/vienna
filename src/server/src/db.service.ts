93ad90c4b0a4572cb46d06fdf4db4a73a7219f7a
ctnmerve <ctnmerve96@gmail.com>
2020-04-13 02:06:32 +0300

    editing  according to mongodb atlas removed

--- src/server/src/db.service.ts
+++ src/server/src/db.service.ts
@@ -1,6 +1,6 @@
-//import mongoose from 'mongoose';
-const MongoClient = require('mongodb').MongoClient;
-/*
+import mongoose from 'mongoose';
+
+
 const DoctorSchema = new mongoose.Schema({
   fullName: String,
   phoneNumber: String,
@@ -8,22 +8,14 @@ const DoctorSchema = new mongoose.Schema({
   isActive: Boolean,
 });
 export const DoctorsCollection = mongoose.model("Doctor", DoctorSchema);
-*/
+
 export class MongoConnection {
 
   public init(username: string, password: string) {
-    const uri = "mongodb+srv://${username}:${password}@vienna-cluster-tzrt1.mongodb.net/test?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-      const collection = client.db("Kovitakip").collection("Doctor");
-  // perform actions on the collection object
-      client.close();
-    });
-
-    /*mongoose.Promise = global.Promise;
+    mongoose.Promise = global.Promise;
     mongoose.connect(`mongodb://${username}:${password}@ds040309.mlab.com:40309/vienna`, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => {
       console.log('MongoConnection established...');
-    });*/
+    });
   }
 
 }
