6cad06fb54f8749256ebdf656bab3e14d40561b3
ctnmerve <ctnmerve96@gmail.com>
2020-04-15 01:29:50 +0300

    MatMenuModule added

--- src/web-client/src/app/theme.module.ts
+++ src/web-client/src/app/theme.module.ts
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatNativeDateModule } from '@angular/material/core';
 import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   exports: [
@@ -20,7 +21,8 @@ import {MatListModule} from '@angular/material/list';
     MatInputModule,
     MatToolbarModule,
     MatNativeDateModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule
   ]
 })
 export class ThemeModule { }
