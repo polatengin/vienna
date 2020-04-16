f6c91e8d34609a36cb9c9bb2db683b2090ce1b80
ctnmerve <ctnmerve96@gmail.com>
2020-04-17 01:42:44 +0300

    DoctorListPageComponent created

---
+++ src/web-client/src/app/pages/doctor-list/doctor-list.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+@Component({
+  selector: 'app-doctor-list',
+  templateUrl: './doctor-list.html',
+  styleUrls: ['./doctor-list.scss']
+})
+export class DoctorListPageComponent {
+doctors;
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Doctor List');
+    layout.showMenu();
+    layout.showBackButton();
+    api.getDoctors()
+    .subscribe(data => this.doctors = data);
+
+    
+  }
+
+  
+
+  
+
+    
+    
+
+  
+
+}
