755e09c4a31f0f94f539d6b135a306f8720cdd95
ctnmerve <ctnmerve96@gmail.com>
2020-04-16 10:28:05 +0300

    connection string changed and timestamps added to schemas

--- src/server/src/db.service.ts
+++ src/server/src/db.service.ts
@@ -6,14 +6,16 @@ const DoctorSchema = new mongoose.Schema({
   phoneNumber: String,
   password: String,
   isActive: Boolean,
-});
+},
+{ timestamps: true });
 const VitalSchema=new mongoose.Schema({
   respiration: String,
   pulse: String,
   temperature: String,
   bloodPressure1: String,
   bloodPressure2: String
-})
+},
+{ timestamps: true });
 
 const PatientSchema=new mongoose.Schema({
   fullName: String,
@@ -22,19 +24,28 @@ const PatientSchema=new mongoose.Schema({
   gender: String,
   email: String,
   patientId: String
-})
+},
+{ timestamps: true });
 
-export const DoctorsCollection = mongoose.model("Doctor", DoctorSchema);
-export const VitalsCollection =mongoose.model("Vital",VitalSchema);
-export const PatientsCollection =mongoose.model("Patient",PatientSchema);
+export const DoctorsCollection = mongoose.model("Doctor", DoctorSchema,"Doctor");
+export const VitalsCollection =mongoose.model("PatientVital",VitalSchema);
+export const PatientsCollection =mongoose.model("Patient",PatientSchema,"Patient");
 
 export class MongoConnection {
 
   public init(username: string, password: string) {
     mongoose.Promise = global.Promise;
-    mongoose.connect(`mongodb://${username}:${password}@ds040309.mlab.com:40309/vienna`, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => {
-      console.log('MongoConnection established...');
-    });
-  }
+    // const uri=`mongodb://${username}:${password}@ds040309.mlab.com:40309/vienna`
+    // mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).then(_ => {
+    //   console.log('MongoConnection established...');
+    // });
+   const dbconnectionstring= `mongodb+srv://${username}:${password}@vienna-cluster-tzrt1.mongodb.net/test?retryWrites=true&w=majority`
+   mongoose.connect(dbconnectionstring, { useNewUrlParser: true,  dbName: 'Kovitakip',useUnifiedTopology: true }).then(_ => {
+    console.log('Mongo atlas Connection established...');
+    DoctorsCollection.findOne({}).then((r) => {
+      console.log(r);
+  })
+  })
 
+}
 }
