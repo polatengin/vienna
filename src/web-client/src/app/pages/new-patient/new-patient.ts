605a691147000b4e713b63215167f8de82992fe9
ctnmerve <ctnmerve96@gmail.com>
2020-04-16 07:13:05 +0300

    page title edited

--- src/web-client/src/app/pages/new-patient/new-patient.ts
+++ src/web-client/src/app/pages/new-patient/new-patient.ts
@@ -16,7 +16,7 @@ export class NewPatientPageComponent {
   request: NewPatientRequestModel = new NewPatientRequestModel();
 
   constructor(layout: LayoutService, private api: ApiService, private router: Router) {
-    layout.updateTitle('New Patient');
+    layout.updateTitle('Patient');
     layout.showMenu();
     layout.showBackButton();
   }
