6a326de788a217123be941a9b05c7603fba3ea87
ctnmerve <ctnmerve96@gmail.com>
2020-04-17 02:55:45 +0300

    specialtyFilter method created

--- src/web-client/src/app/pages/doctor-list/doctor-list.ts
+++ src/web-client/src/app/pages/doctor-list/doctor-list.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { ApiService } from '../../services/api.service';
 import { LayoutService } from '../../services/layout.service';
+import { Specialty } from '../../models/doctor';
 
 @Component({
   selector: 'app-doctor-list',
@@ -11,13 +12,23 @@ import { LayoutService } from '../../services/layout.service';
 })
 export class DoctorListPageComponent {
 doctors;
+copyDoctors;
 filterText:any;
+specialty=Specialty;
+specialties;
+option = 'Tümü';
+
   constructor(layout: LayoutService, private api: ApiService, private router: Router) {
     layout.updateTitle('Doctor List');
     layout.showMenu();
     layout.showBackButton();
     api.getDoctors()
-    .subscribe(data => this.doctors = data);
+    .subscribe(data =>{
+        this.doctors = data;
+        this.copyDoctors=this.doctors.slice();
+    });
+
+    this.specialties=Object.values(this.specialty);
 
     
   }
@@ -26,7 +37,13 @@ filterText:any;
     this.router.navigate(['/new-doctor']);
   }
 
-  
+  specialtyFilter(filterOption: any) {
+    if (filterOption == '0') {
+     this.doctors = this.copyDoctors;
+    } else {
+     this.doctors = this.copyDoctors.filter((item) => item.specialty === filterOption);
+    }
+   }
 
     
     
