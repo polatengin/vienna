9920e6f4113ce94ed34e9aaa66e0a7ce4d70703d
ctnmerve <ctnmerve96@gmail.com>
2020-04-16 20:41:45 +0300

    NewDoctorPageComponent created

---
+++ src/web-client/src/app/pages/new-doctor/new-doctor.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+import { Doctor, Specialty } from '../../models/doctor';
+
+@Component({
+  selector: 'app-new-doctor',
+  templateUrl: './new-doctor.html',
+  styleUrls: ['./new-doctor.scss']
+})
+export class NewDoctorPageComponent {
+
+  request: Doctor = new Doctor();
+  specialty=Specialty;
+  specialties;
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Doctor');
+    layout.showMenu();
+    layout.showBackButton();
+    this.specialties=Object.values(this.specialty);
+
+  }
+
+
+
+}
