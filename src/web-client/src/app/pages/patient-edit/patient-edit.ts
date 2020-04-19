9254ffc8485732f60b312b9599ba166bd3354d92
ctnmerve <ctnmerve96@gmail.com>
2020-04-19 03:59:33 +0300

    PatientEditPageComponent created

---
+++ src/web-client/src/app/pages/patient-edit/patient-edit.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+import { NewPatientRequestModel } from '../../models/api-models';
+
+
+
+@Component({
+  selector: 'app-patient-edit',
+  templateUrl: './patient-edit.html',
+  styleUrls: ['./patient-edit.scss']
+})
+export class PatientEditPageComponent {
+
+    request: NewPatientRequestModel = new NewPatientRequestModel();
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Edit Patient');
+    layout.showMenu();
+    layout.showBackButton();
+  }
+
+  
+
+}
