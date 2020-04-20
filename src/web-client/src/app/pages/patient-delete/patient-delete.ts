bd2264c2868e0cb988c5c83f86d6f131b8a74198
ctnmerve <ctnmerve96@gmail.com>
2020-04-20 10:31:16 +0300

    PatientDeletePageComponent created

---
+++ src/web-client/src/app/pages/patient-delete/patient-delete.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+
+
+
+@Component({
+  selector: 'app-patient-delete',
+  templateUrl: './patient-delete.html',
+  styleUrls: ['./patient-delete.scss']
+})
+export class PatientDeletePageComponent {
+
+  
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Delete Patient');
+    layout.showMenu();
+    layout.showBackButton();
+    
+    
+  }
+
+  
+
+}
