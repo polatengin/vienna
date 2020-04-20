65bee85dada1a489a9de02d5a9943c708fbcb622
ctnmerve <ctnmerve96@gmail.com>
2020-04-20 06:19:14 +0300

    VitalDeletePageComponent created

---
+++ src/web-client/src/app/pages/vital-delete/vital-delete.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+
+
+
+@Component({
+  selector: 'app-vital-delete',
+  templateUrl: './vital-delete.html',
+  styleUrls: ['./vital-delete.scss']
+})
+export class VitalDeletePageComponent {
+
+  vital;
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Delete Vital');
+    layout.showMenu();
+    layout.showBackButton();
+    this.api.getVital()
+    .subscribe(data => this.vital = data);
+    
+  }
+
+  delete() {
+    this.api.deleteVital(this.api.vitalId);
+    this.router.navigate(['/vital/'+this.api.patientId]);
+  }
+
+}
