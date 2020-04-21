eb3be9ce979226b5fd0ef01bf6476398927ebb71
ctnmerve <ctnmerve96@gmail.com>
2020-04-21 19:33:34 +0300

    MedicationListPageComponent created

---
+++ src/web-client/src/app/pages/medication-list/medication-list.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../services/api.service';
+import { LayoutService } from '../../services/layout.service';
+
+@Component({
+  selector: 'app-medication-list',
+  templateUrl: './medication-list.html',
+  styleUrls: ['./medication-list.scss']
+})
+export class MedicationListPageComponent {
+medications;
+
+  constructor(layout: LayoutService, private api: ApiService, private router: Router) {
+    layout.updateTitle('Medication List');
+    layout.showMenu();
+    layout.showBackButton();
+    api.getMedications()
+    .subscribe(data => this.medications = data);
+    
+
+    
+  }
+
+  gotoAddMedicationPage() {
+    this.router.navigate(['/medication/add']);
+  }
+
+ 
+
+}
