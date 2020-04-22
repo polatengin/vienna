a5761c766c853a2af293680f49e03dcf98c9046f
ctnmerve <ctnmerve96@gmail.com>
2020-04-23 01:19:50 +0300

    complaint enum created

---
+++ src/web-client/src/app/models/diseases.ts
@@ -0,0 +1,12 @@
+export enum Complaint {
+    Ateş='Ateş',
+    Oksürük='Öksürük',
+    Halsizlik='Halsizlik',
+    NefesDarlıgı='Nefes Darlığı',
+    BogazAgrısı='Boğaz Ağrısı',
+    BasAgrısı='Baş Ağrısı',
+    GögüsSıkısması='Göğüs Sıkışması',
+    BurunAkıntısı='Burun Akıntısı',
+    VücutAgrıları='Vücut Ağrıları',
+    İshalKusma='İshal-Kusma'
+}
